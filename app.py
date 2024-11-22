4d339debd0e5f0d344031ae8faa5b14c4242360c
Aanis Ali Shah <33195513+alight659@users.noreply.github.com>
2024-11-22 12:45:06 +0530

    Feature Added: API

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, flash, render_template, request, redirect, session, Response
+from flask import Flask, flash, render_template, request, redirect, session, Response, jsonify
 from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
@@ -39,13 +39,13 @@ def loginData():
 # Error Handling 404
 @app.errorhandler(404)
 def error(code):
-    return render_template("error.html", code=code)
+    return render_template("error.html", code=code), 404
 
 
 # Error Handling 500
 @app.errorhandler(500)
 def error(code):
-    return render_template("error.html", code=code)
+    return render_template("error.html", code=code), 500
 
 
 # Main Index Function
@@ -150,7 +150,7 @@ def clip(clip_url_id):
             return render_template("clip.html", url_id=clip_url_id, name=name, text=text, time=time, edit=is_editable, update=updated, ext=ext, dat=loginData())
 
     else:
-        return render_template("error.html", code="That was not found on this server.")
+        return render_template("error.html", code="That was not found on this server."), 404
 
 
 # Search Function
@@ -237,10 +237,10 @@ def login():
         uname = request.form.get("uname")
         passwd = request.form.get("passwd")
         if not uname:
-            return render_template("error.html", code="Username cannot be empty.")
+            flash("Username Cannot be Empty!")
 
         if not passwd:
-            return render_template("error.html", code="Password cannot be empty.")
+            flash("Password Cannot be Empty!")
 
         data = db.execute("SELECT * FROM users WHERE username=?", uname)
         if len(data) != 0:
@@ -248,10 +248,12 @@ def login():
                 session["user_id"] = data[0]["id"]
                 session["uname"] = uname
                 return redirect("/")
+            else:
+                flash("Incorrect Username or Password!")
         else:
             flash("Account Not Found!")
 
-    return render_template("login.html")
+    return render_template("login.html", dat=loginData(), reg=True)
 
 
 # Logout Function
@@ -264,6 +266,9 @@ def logout():
 # Registration Function
 @app.route("/register", methods=["GET", "POST"])
 def register():
+
+    session.clear()
+
     if request.method == "POST":
         uname = request.form.get("uname")
         passwd = request.form.get("passwd")
@@ -287,16 +292,104 @@ def register():
         db.execute("INSERT INTO users (username, password) VALUES (?, ?)", uname, generate_password_hash(passwd))
 
         return redirect("/login")
-    return render_template("register.html")
+    return render_template("register.html", dat=loginData())
 
 
 # Dashboard Function
 @app.route("/dashboard")
+@app.route("/dashboard/")
 @login_required
 def dashboard():
     uname = loginData()[1]
     data = db.execute("SELECT clips.clip_name, clips.clip_url, clips.clip_time, clips.is_editable, clips.is_unlisted FROM clipRef JOIN clips ON clips.id = clipRef.clipid JOIN users ON users.id = clipRef.userid WHERE users.username=?", session["uname"])
     return render_template("dash.html", dat=loginData(), data=data)
+
+
+# API Get Function
+@app.route("/api/get_data")
+def get_data():
+    clip_id = request.args.get("id")
+    clip_name = request.args.get("name")
+    clip_pass = request.args.get("pwd")
+    unlisted = request.args.get("unlisted")
+    
+    data = {}
+
+    if not clip_id and not clip_name:
+        return jsonify({'Error': 'Missing Parameters!'}), 400
+
+    if clip_name and unlisted == 'true':
+        return jsonify({'Message': 'To Search, enter Id not Name with unlisted!'}), 400
+
+    if clip_name:
+        clip_name = '%'+clip_name+'%'
+    
+    if not clip_pass:
+        if unlisted == 'true' and clip_id:
+            data = db.execute("SELECT clip_url, clip_name, clip_text, clip_time, clip_pwd FROM clips WHERE (clip_url LIKE ?) AND is_unlisted == 1 AND clip_pwd IS NULL", str(clip_id))
+        else:
+            data = db.execute("SELECT clip_url, clip_name, clip_text, clip_time, clip_pwd FROM clips WHERE (clip_url LIKE ? OR clip_name LIKE ?) AND is_unlisted != 1 AND clip_pwd IS NULL", str(clip_id), str(clip_name))
+    else:
+        if unlisted == 'true' and clip_id:
+            data = db.execute("SELECT clip_url, clip_name, clip_text, clip_time, clip_pwd FROM clips WHERE (clip_url LIKE ?) AND is_unlisted == 1", str(clip_id))
+        else:
+            data = db.execute("SELECT clip_url, clip_name, clip_text, clip_time, clip_pwd FROM clips WHERE (clip_url LIKE ? OR clip_name LIKE ?) AND is_unlisted != 1", str(clip_id), str(clip_name))
+        if data[0]['clip_pwd'] != None:
+            if not check_password_hash(data[0]['clip_pwd'], clip_pass):
+                return jsonify({'Error': 'Incorrect Password'}), 401
+        
+    
+    datl = []
+    if len(data) != 0:
+        for i in data:
+            dat = {}
+            dat['id'] = i['clip_url']
+            dat['name'] = i['clip_name']
+            dat['text'] = i['clip_text']
+            dat['time'] = i['clip_time']
+            datl.append(dat)
+        return jsonify(datl)
+    return jsonify({'Error': 'No Data'}), 404
+
+
+# API POST Function
+@app.route("/api/post_data", methods=["GET","POST"])
+def post_data():
+    clip_name = request.form.get("name")
+    clip_id = gen_id()
+    clip_text = request.form.get("text")
+    unlist = request.form.get("unlisted")
+    clip_pass = request.form.get("pwd")
+
+    if not clip_name or not clip_text:
+        return jsonify({'Error': 'Missing Parameters!'}), 400
+
+    if unlist:
+        if unlist == 'true':
+            unlist = 1
+        else:
+            unlist = 0
+    else:
+        unlist = 0
+
+    successStatus = False
+    if not clip_pass:
+        db.execute("INSERT INTO clips (clip_url, clip_name, clip_text, is_editable, is_unlisted, clip_time) VALUES (?,?,?,0,?, datetime('now', 'localtime'))", str(clip_id), str(clip_name), str(clip_text), unlist)
+        successStatus = True
+    elif clip_pass:
+        pwd = generate_password_hash(clip_pass)
+        db.execute("INSERT INTO clips (clip_url, clip_name, clip_text, clip_pwd, is_editable, is_unlisted, clip_time) VALUES (?,?,?,?,0,?, datetime('now', 'localtime'))", str(clip_id), str(clip_name), str(clip_text), pwd, unlist)
+        successStatus = True
+    if successStatus:
+        return jsonify({'id': str(clip_id),'Message': 'Successfully added!'}), 201
+    return jsonify({'Error': 'Couldn\'t add. Something went wrong.'}), 400
+
+
+# API Documentation Page
+@app.route("/api")
+@app.route("/api/")
+def api():
+    return render_template("api.html", dat=loginData())
 
 
 if __name__ == "__main__":
